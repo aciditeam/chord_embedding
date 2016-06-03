39c0f60d6f741a988be7e6eae8c837616a21cd28
Leo Crestel Mac <crestel.leopold@gmail.com>
2016-06-03 17:21:49 +0200

    working version of trace and tSNE !

---
+++ Code/tSNE.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# Bokeh
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool
+from collections import OrderedDict
+from bokeh.plotting import *
+
+# SKlearn
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+# Numpy
+import numpy as np
+import unicodecsv as csv
+import re
+import colorsys
+
+COMPUTE = True
+
+# Get data
+print "Load data"
+output_file("../Results/tsne.html")
+data_path = "../Results/data.csv"
+data = np.transpose(np.loadtxt(data_path, delimiter=','))
+
+# load the change track matrix
+time_pointer_path = "../final_data/time_pointer2.csv"
+time_pointer = np.loadtxt(time_pointer_path, delimiter=',')
+
+# Beginning of tracks in the new event granularity : tp_ne
+event_total_path = "../final_data/event_total2.csv"
+event_total = np.loadtxt(event_total_path, delimiter=',')
+
+
+def get_tp_ne():
+    tp_ne = np.zeros((time_pointer.shape[0]-1))  # Last item is useless (end of the last track)
+    current_track = 0
+    event_counter = 0
+    for index in range(event_total.shape[0]):
+        if event_total[index]:
+            event_counter += 1
+        if index == time_pointer[current_track]+1:
+            tp_ne[current_track] = event_counter
+            current_track += 1
+    return tp_ne
+tp_ne = get_tp_ne()
+
+
+def map_color(e, N):
+    hue = float(e) / (N+1)
+    saturation = float(180 + 90*(float(e) / (N+1))) / 360
+    lightness = float(200. / 360)
+    r, g, b = colorsys.hls_to_rgb(hue, lightness, saturation)
+    return '#%02x%02x%02x' % (int(r * 256), int(g * 256), int(b * 256))
+
+# Compute reduction : mask data with event_total
+event_total = event_total.astype(np.int)
+data_ne = data[event_total, :]
+
+data_ne = data_ne[:500,:]
+
+# # Compute PCA
+# print "Compute PCA"
+# pca = PCA(n_components=50)
+# data_pca = pca.fit_transform(data)
+# Compute t-SNE
+if __name__ == '__main__':
+    if COMPUTE:
+        print "Compute t-SNE"
+        tsne = TSNE(n_components=2, random_state=0, n_iter=200)
+        data_reduced = tsne.fit_transform(data_ne)
+    else:
+        data_reduced = np.loadtxt("tsne.csv", delimiter=",")
+
+    # Labelling and colours
+    # Save data
+    np.savetxt("tsne.csv", data_reduced, delimiter=',')
+
+    # Build list name
+    file_names = []
+    extension = ur"\.mid$"
+    with open("../final_data/track_list2.csv", "rb") as f:
+        reader = csv.reader(f, delimiter=";")
+        for row in reader:
+            fname_no_extension = re.sub(extension, "", row[0], re.U)
+            fname_no_parenthesis = re.sub(ur"\(.*$", "", fname_no_extension, re.U)
+            file_names.append(fname_no_parenthesis)
+
+    # Colors list
+    num_track = len(file_names)
+    colors = [map_color(e, num_track) for e in range(len(file_names))]
+
+    # Define a function for labelling the data
+    def labelling(time, tp):
+        i = 0
+        while(tp[i] <= time):
+            i += 1
+        return file_names[i], colors[i]
+
+    # Plot t-SNE map
+    print "Plot data"
+    TOOLS="crosshair,pan,wheel_zoom,box_zoom,reset,hover,previewsave"
+
+    source = ColumnDataSource(
+        data=dict(
+            x=data_reduced[:,0],
+            y=data_reduced[:,1],
+            label=[labelling(e, tp_ne)[0] for e in range(data_reduced.shape[0])],
+            color=[labelling(e, tp_ne)[1] for e in range(data_reduced.shape[0])]
+        )
+    )
+
+    p = figure(title="Chord embedding # t-SNE visualization", tools=TOOLS)
+    p.circle('x', 'y', color='color', line_width=2, source=source)
+
+    hover = p.select(dict(type=HoverTool))
+    hover.tooltips = OrderedDict([
+        # ("index", "$index"),
+        ("(x,y)", "(@x, @y)"),
+        ("piece", "@label"),
+    ])
+
+    show(p)
--- Code/trace.py
+++ Code/trace.py
@@ -2,80 +2,97 @@
 # -*- coding: utf8 -*-
 
 # Bokeh
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, show
 from bokeh.models import HoverTool
 from collections import OrderedDict
 from bokeh.plotting import *
 
-# SKlearn
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
 # Numpy
 import numpy as np
 import unicodecsv as csv
-import re
-import colorsys
 
+import re
+
+import tSNE
+
+track_names = ["Haydn_Symph104_ii(1-8)_ORCH+REDUC+piano.mid", "Haydn_Symph104_ii(1-8)_ORCH+REDUC+piano.mid", "Haydn_Symph104_ii(1-8)_ORCH+REDUC+piano.mid"]
 trace_length = 20
+ratio_tracks = [0.1, 0.2, 0.4]  # Portion track
+# Color list
+num_track = len(track_names)
+colors = colors = [tSNE.map_color(e, num_track) for e in range(len(track_names))]
 
 # Load t-SNE
 print "Load t-SNE"
 data = np.loadtxt("tsne.csv", delimiter=",")
 
-# Labelling and colours
-# load the change track matrix
-time_pointer_path = "../final_data/time_pointer2.csv"
-time_pointer = np.loadtxt(time_pointer_path, delimiter=',')
-# Build list name
+# Get files names
 file_names = []
-extension = r"\.mid$"
 with open("../final_data/track_list2.csv", "rb") as f:
     reader = csv.reader(f, delimiter=";")
     for row in reader:
-        fname_no_extension = re.sub(extension, "", row)
-        fname_no_parenthesis = re.sub(r"\(.*$", "", fname_no_extension)
-        file_names.append(fname_no_parenthesis)
+        file_names.append(row[0])
 
+# New track indices
+tp_ne = tSNE.get_tp_ne()
 
-# Colors list
-num_track = len(file_names)
-def map_color(e):
-    hue = float(e) / (num_track+1)
-    saturation = float(180. / 360)
-    lightness = float(200. / 360)
-    r, g, b = colorsys.hls_to_rgb(hue, lightness, saturation)
-    return '#%02x%02x%02x' % (int(r * 256), int(g * 256), int(b * 256))
-colors = [map_color(e) for e in range(len(file_names))]
-
-# Define a function for labelling the data
-def labelling(time):
-    i = 0
-    while(time_pointer[i] <= time):
-        i += 1
-    return file_names[i], colors[i]
-
-
-# Plot t-SNE map
+# Plot path
 print "Plot data"
 TOOLS="crosshair,pan,wheel_zoom,box_zoom,reset,hover,previewsave"
+p = figure(title="Trace of time series in t-SNE space. {} points".format(trace_length), tools=TOOLS)
+for track_name, ratio_track, color in zip(track_names, ratio_tracks, colors):
+    # Extract t-SNE path for the requested file
+    # Get index from file name
+    index_file = None
+    for num, file_name in enumerate(file_names):
+        fname = re.split(ur"/", file_name, flags=re.I|re.U)[-1]
+        if fname == track_name:
+            index_file = num
+            break
 
-p = figure(title="Embedding for similarity/dissimilarity (reduced to 2 dim with t-SNE)", tools=TOOLS)
+    if index_file == 0:
+        # First track
+        start_track = 0
+    else:
+        start_track = int(tp_ne[index_file])
+    if index_file == len(file_names):
+        # Last track
+        end_track = data.shape[0]
+    else:
+        end_track = int(tp_ne[index_file])
 
-for
-source = ColumnDataSource(
-    data=dict(
-        x=data[:,0],
-        y=data[:, 1],
-        label=[labelling(e)[0] for e in range(data.shape[0])]
+    # Extract track
+    data_track = data[start_track:end_track,:]
+    # Extract path
+    N_track = data_track.shape[0]
+    start_path = int(ratio_track*N_track)
+    end_path = min(start_path+trace_length, N_track)
+    path = data_track[start_path:end_path]
+
+    extension = ur"\.mid$"
+    fname_no_extension = re.sub(extension, "", row[0], re.U)
+    fname_no_parenthesis = re.sub(ur"\(.*$", "", fname_no_extension, re.U)
+
+    source = ColumnDataSource(
+        data=dict(
+            x=path[:,0],
+            y=path[:,1],
+            label=[fname_no_parenthesis for t in range(path.shape[0])],
+            ratio=[str(ratio_track) for t in range(path.shape[0])],
+            time=[t for t in range(path.shape[0])]
+        )
     )
-)
-p.circle('x', 'y', color='color', line_width=2, source=source)
 
-hover = p.select(dict(type=HoverTool))
-hover.tooltips = OrderedDict([
-    # ("index", "$index"),
-    ("(x,y)", "(@x, @y)"),
-    ("piece", "@label"),
-])
+    p.line('x', 'y', color=color, line_width=2, source=source)
+    p.circle('x', 'y', color=color, line_width=2, source=source)
+
+    hover = p.select(dict(type=HoverTool))
+    hover.tooltips = OrderedDict([
+        # ("index", "$index"),
+        ("(x,y)", "(@x, @y)"),
+        ("label", "@label"),
+        ("ratio", "@ratio"),
+        ("time", "@time"),
+    ])
 
 show(p)
